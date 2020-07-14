7ea62fe2c11075ebc610aeabd54ddc2e3cadd8b3
Alexander Kornev <headholder25@gmail.com>
2020-07-14 18:50:30 +0300

    Создан словарь словарей из городов Moscow, London, Paris и расстояний между ними.

--- lesson_002/00_distance.py
+++ lesson_002/00_distance.py
@@ -3,6 +3,8 @@
 
 # Есть словарь координат городов
 
+from pprint import pprint
+
 sites = {
     'Moscow': (550, 370),
     'London': (510, 510),
@@ -12,12 +14,33 @@ sites = {
 # Составим словарь словарей расстояний между ними
 # расстояние на координатной сетке - ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5
 
-distances = {}
+distances = dict()
 
-# TODO здесь заполнение словаря
+moscow = sites['Moscow']
+london = sites['London']
+paris = sites['Paris']
+moscow_london = ((moscow[0]-london[0])**2 + (moscow[1] - london[1]) ** 2) ** 0.5
+moscow_paris = ((moscow[0]-paris[0])**2 + (moscow[1] - paris[1]) ** 2) ** 0.5
 
-print(distances)
+distances['Moscow'] = {}
+distances['Moscow']['London'] = moscow_london
+distances['Moscow']['Paris'] = moscow_paris
 
+london_moscow = ((london[0]-moscow[0])**2 + (london[1] - moscow[1]) ** 2) ** 0.5
+london_paris = ((london[0]-paris[0])**2 + (london[1] - paris[1]) ** 2) ** 0.5
+
+distances['London'] = {}
+distances['London']['Moscow'] = london_moscow
+distances['London']['Paris'] = london_paris
+
+paris_moscow = ((paris[0]-moscow[0])**2 + (paris[1] - moscow[1]) ** 2) ** 0.5
+paris_london = ((paris[0]-london[0])**2 + (paris[1] - london[1]) ** 2) ** 0.5
+
+distances['Paris'] = {}
+distances['Paris']['Moscow'] = paris_moscow
+distances['Paris']['London'] = paris_london
+
+pprint(distances)
 
 
 
