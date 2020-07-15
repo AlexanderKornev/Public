8cb89a3de2ef887e0222e522e709564ed5581680
Alexander Kornev <headholder25@gmail.com>
2020-07-15 18:31:49 +0300

    Добавил переменные для store[goods['название предмета мебели']] для сокращения кода.

--- lesson_002/10_store.py
+++ lesson_002/10_store.py
@@ -51,17 +51,28 @@ print('Лампа -', lamps_quantity, 'шт, стоимость', lamps_cost, '�
 
 # WARNING для знающих циклы: БЕЗ циклов. Да, с переменными; да, неэффективно; да, копипаста.
 # Это задание на ручное вычисление - что бы потом понять как работают циклы и насколько с ними проще жить.
+# table - сокращение переменной
 
-table_cost = store[goods['Стол']][0]['quantity'] * store[goods['Стол']][0]['price'] + store[goods['Стол']][1]['quantity'] * store[goods['Стол']][1]['price']
-table_quantity = store[goods['Стол']][0]['quantity'] + store[goods['Стол']][1]['quantity']
+table = store[goods['Стол']]
+
+table_cost = table[0]['quantity'] * table[0]['price'] + table[1]['quantity'] * table[1]['price']
+table_quantity = table[0]['quantity'] + table[1]['quantity']
 print('Стол -', table_quantity , 'шт, стоимость', table_cost, 'руб')
 
-couch_cost = store[goods['Диван']][0]['quantity'] * store[goods['Диван']][0]['price'] + store[goods['Диван']][1]['quantity'] * store[goods['Диван']][1]['price']
-couch_quantity = store[goods['Диван']][0]['quantity'] + store[goods['Диван']][1]['quantity']
+# couch - сокращение переменной
+
+couch = store[goods['Диван']]
+
+couch_cost = couch[0]['quantity'] * couch[0]['price'] + couch[1]['quantity'] * couch[1]['price']
+couch_quantity = couch[0]['quantity'] + couch[1]['quantity']
 print('Диван -', couch_quantity, 'шт, стоимость', couch_cost, 'руб')
 
-chair_cost = store[goods['Стул']][0]['quantity'] * store[goods['Стул']][0]['price'] + store[goods['Стул']][1]['quantity'] * store[goods['Стул']][1]['price'] + store[goods['Стул']][2]['quantity'] * store[goods['Стул']][2]['price']
-chair_quantity = store[goods['Стул']][0]['quantity'] + store[goods['Стул']][1]['quantity'] + store[goods['Стул']][2]['quantity']
+# chair - сокращение переменной
+
+chair = store[goods['Стул']]
+
+chair_cost = chair[0]['quantity'] * chair[0]['price'] + chair[1]['quantity'] * chair[1]['price'] + chair[2]['quantity'] * chair[2]['price']
+chair_quantity = chair[0]['quantity'] + chair[1]['quantity'] + chair[2]['quantity']
 print('Стул -', chair_quantity, 'шт, стоимость', chair_cost, 'руб')
 ##########################################################################################
 # ВНИМАНИЕ! После того как __ВСЯ__ домашняя работа сделана и запушена на сервер,         #
