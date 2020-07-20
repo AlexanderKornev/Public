2cfdbcff5f0f85c8e26aefe6621fc2e249e09cff
Alexander Kornev <headholder25@gmail.com>
2020-07-20 10:56:02 +0300

    Нарисовал семь линий разного цвета, получиалсь радуга. Выполнил задание под звездочкой, нарисовал более "естественную" радугу. Второй вариант (как в решении).

--- lesson_003/06_rainbow.py
+++ lesson_003/06_rainbow.py
@@ -11,16 +11,16 @@ rainbow_colors = (sd.COLOR_RED, sd.COLOR_ORANGE, sd.COLOR_YELLOW, sd.COLOR_GREEN
 
 # Нарисовать радугу: 7 линий разного цвета толщиной 4 с шагом 5 из точки (50, 50) в точку (350, 450)
 
-point_x = 50
-end_point_x = 350
-
-
-for _ in rainbow_colors:
-    point = sd.get_point(point_x, 50)
-    end_point = sd.get_point(end_point_x, 450)
-    sd.line(start_point=point, end_point=end_point, color=_, width=4)
-    point_x += 5
-    end_point_x += 5
+# point_x = 50
+# end_point_x = 350
+#
+#
+# for _ in rainbow_colors:
+#     point = sd.get_point(point_x, 50)
+#     end_point = sd.get_point(end_point_x, 450)
+#     sd.line(start_point=point, end_point=end_point, color=_, width=4)
+#     point_x += 5
+#     end_point_x += 5
 
 
 # Подсказка: цикл нужно делать сразу по тьюплу с цветами радуги.
@@ -29,7 +29,7 @@ for _ in rainbow_colors:
 # Усложненное задание, делать по желанию.
 # Нарисовать радугу дугами от окружности (cсм sd.circle) за нижним краем экрана,
 # поэкспериментировать с параметрами, что бы было красиво
-point = sd.get_point(-20, -100)
+point = sd.get_point(602, -100)
 radius = 500
 for _ in rainbow_colors[::-1]:
     radius += 16
