5e7bb503efffc1dc0b0de3e6a798a237e177155c
Alexander Kornev <headholder25@gmail.com>
2020-07-28 12:58:36 +0300

    Спасибо за ТУДУ! Код стал гораздо лаконичней! Убрал вложенные циклы. Сделал словарь списков для вариантов раскраски.

--- lesson_004/02_global_color.py
+++ lesson_004/02_global_color.py
@@ -18,20 +18,13 @@ import simple_draw as sd
 def vector(point, length, angle=0, lines=0, color=sd.COLOR_RED):
     begin_point = point
     next_angle = 360 // lines
-    shape_count = 0
-    shape_for_line = lines - 1
-    for _ in range(lines):
-        if shape_count < shape_for_line:
-            v = sd.get_vector(start_point=point, angle=angle, length=length, width=1)
-            v.draw()
-            # TODO, line как переменную создавать не нужно =) она всегда рисуется сразу.
-            line_v = sd.line(start_point=v.start_point, end_point=v.end_point, width=1, color=color)
-            point = v.end_point
-            angle = v.angle + next_angle
-            shape_count = shape_count + 1
-        else:
-            # TODO, line как переменную создавать не нужно =) она всегда рисуется сразу.
-            l3 = sd.line(start_point=v.end_point, end_point=begin_point, width=1, color=color)
+    for _ in range(lines-1):
+        v = sd.get_vector(start_point=point, angle=angle, length=length, width=1)
+        v.draw()
+        sd.line(start_point=v.start_point, end_point=v.end_point, width=1, color=color)
+        point = v.end_point
+        angle = v.angle + next_angle
+    sd.line(start_point=v.end_point, end_point=begin_point, width=1, color=color)
 
 
 def triangle(point, length, angle, color):
@@ -52,42 +45,36 @@ def hexagon(point, length, angle, color):
 
 # Пожалуйста, обратите внимание на перенос текста.
 color_dic = {
-    'red': sd.COLOR_RED,
-    'orange': sd.COLOR_ORANGE,
-    'yellow': sd.COLOR_YELLOW,
-    'green': sd.COLOR_GREEN,
-    'cyan': sd.COLOR_CYAN,
-    'blue': sd.COLOR_BLUE,
-    'purple': sd.COLOR_PURPLE
+    '0': ['red', sd.COLOR_RED],
+    '1': ['orange', sd.COLOR_ORANGE],
+    '2': ['yellow', sd.COLOR_YELLOW],
+    '3': ['green', sd.COLOR_GREEN],
+    '4': ['cyan', sd.COLOR_CYAN],
+    '5': ['blue', sd.COLOR_BLUE],
+    '6': ['purple', sd.COLOR_PURPLE],
 }
 
-# TODO, Александр, пожалуйста. Обратите внимание. Т.к. мы используем словарь.
-#  Ответ пользователя приводить к int не нужно. Лучше, ключами в словаре использовать числа в формате строки,
-#  которые может ввести пользователь. ("0", "1" и т.д.) В таком случае название цвета и сам цвет будут как значения.
-#  Их можно объеденить в список или в ещё один словарь. Получится Словарь списков или словарь словарей =).
+
 while True:
-    print('Возможные цвета: ')
-    for number, color_print in enumerate(color_dic):
-        print(number, ':', color_print)
-    user_color = int(input('Введите желаемый цвет: '))
-    if 0 < user_color < 6:
-        for color_number, color_color in enumerate(color_dic):
-            if color_number == user_color:
-                #  TODO, получилось много вложенных циклов и условий. После реализации первой части ТУДУ
-                #   они должны уйти =) Предлагаю оставить переменную color в цикле while,
-                #   а отрисовку делать уже после, когда перменная создана.
-                color = color_dic[color_color]
-                triangle_point = sd.get_point(100, 100)
-                triangle(point=triangle_point, length=100, angle=0, color=color)
-                point_square = sd.get_point(400, 100)
-                square(point=point_square, length=100, angle=0, color=color)
-                point_pentagon = sd.get_point(100, 400)
-                pentagon(point=point_pentagon, length=100, angle=0, color=color)
-                point_hexagon = sd.get_point(400, 400)
-                hexagon(point=point_hexagon, length=100, angle=0, color=color)
-        break
-    elif 6 > user_color < 0:
+    for number_color, number_name_color in color_dic.items():
+        color_name = number_name_color[0]
+        print(number_color, ':', color_name)
+    user_color = input('Введите желаемый цвет: ')
+    if int(user_color) > 6:
         print('Вы ввели некорректный номер!')
+    else:
+        color = color_dic[user_color][1]
+        triangle_point = sd.get_point(100, 100)
+        triangle(point=triangle_point, length=100, angle=0, color=color)
+        point_square = sd.get_point(400, 100)
+        square(point=point_square, length=100, angle=0, color=color)
+        point_pentagon = sd.get_point(100, 400)
+        pentagon(point=point_pentagon, length=100, angle=0, color=color)
+        point_hexagon = sd.get_point(400, 400)
+        hexagon(point=point_hexagon, length=100, angle=0, color=color)
+        break
+    # elif 6 > user_color < 0:
+    #     print('Вы ввели некорректный номер!')
 
 # triangle_point = sd.get_point(100, 100)
 # triangle(point=triangle_point, length=100, angle=0)
