f595d459b5d3ef6719ba9445f07038f6484ce5d9
Alexander Kornev <headholder25@gmail.com>
2020-07-29 15:54:49 +0300

    Передалал словарь! Переделал проверку вводимого пользователем через in.

--- lesson_004/03_shape_select.py
+++ lesson_004/03_shape_select.py
@@ -19,12 +19,11 @@ def vector(point, length, angle=0, lines=0, color=sd.COLOR_RED):
         if shape_count < shape_for_line:
             v = sd.get_vector(start_point=point, angle=angle, length=length, width=1)
             v.draw()
-            line_v = sd.line(start_point=v.start_point, end_point=v.end_point, width=1, color=color)
             point = v.end_point
             angle = v.angle + next_angle
             shape_count = shape_count + 1
         else:
-            l3 = sd.line(start_point=v.end_point, end_point=begin_point, width=1, color=color)
+            sd.line(start_point=v.end_point, end_point=begin_point, width=1, color=color)
 
 
 def triangle(point, length, angle):
@@ -42,20 +41,14 @@ def pentagon(point, length, angle):
 def hexagon(point, length, angle):
     vector(point=point, angle=angle, length=length, lines=6)
 
-# TODO, Александр, пожалуйста, обратите внимание, как можно зранить функции в словаре и как вызывать их.
-#  example_figure_dic = {
-#      '0': ['треугольник', triangle],
-#  }
-#  example_func = example_figure_dic["0"][1]  # Создаём функцию
-#  example_func()  # Вызываем функцию
 
-# TODO, пожалуйста переделайте этот словарь =)
 figure_dic = {
-    '0': ['треугольник', 3],
-    '1': ['квадрат', 4],
-    '2': ['пятиугольник', 5],
-    '3': ['шестиугольник', 6]
+    '0': ['треугольник', triangle],
+    '1': ['квадрат', square],
+    '2': ['пятиугольник', pentagon],
+    '3': ['шестиугольник', hexagon]
 }
+
 
 while True:
     print('Фигуры: ')
@@ -63,15 +56,13 @@ while True:
         figure_name = figure_dic[number][0]
         print(number, ':', figure_name)
     user_figure = input('Введите желаемую фигуру: ')
-    # TODO, Александр, давайте в этом месте так же проверять. Является ли то,
-    #  что ввёл пользователь ключём нашего словаря. Если да, то создаём функцию. Вызываем её уже вне цикла.
-    #  Цикл нужен только для проерки введёного пользователем и создания функции.
-    if int(user_figure) > 3:
-        print('Вы ввели некорректный номер!')
-    else:
-        lines = figure_dic[user_figure][1]
-        center = sd.get_point(200, 200)
-        vector(point=center, length=100, angle=0, lines=lines, color=sd.COLOR_RED)
+    if user_figure in figure_dic:
+        figure = figure_dic[user_figure][1]
+        point = sd.get_point(300, 300)
+        figure(point, 100, 0)
         break
+    else:
+        print('Вы ввели некорректный номер!')
+
 
 sd.pause()
