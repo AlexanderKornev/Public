643a662032308a2f1b7f0315da4e1098cc6819bf
Alexander Kornev <headholder25@gmail.com>
2020-08-01 17:23:11 +0300

    Красивый вывод.

--- lesson_005/01_inhabitants.py
+++ lesson_005/01_inhabitants.py
@@ -2,9 +2,11 @@
 
 # Вывести на консоль жителей комнат (модули room_1 и room_2)
 # Формат: В комнате room_1 живут: ...
+from os.path import join
 
 import lesson_005.room_1 as r1
 import lesson_005.room_2 as r2
+comma = ', '
+print('В комнате room_1 живут:', s.join(r1.folks))
+print('В комнате room_2 живут:', s.join(r2.folks))
 
-print('В комнате room_1 живут:', r1.folks)
-print('В комнате room_2 живут:', r2.folks)
