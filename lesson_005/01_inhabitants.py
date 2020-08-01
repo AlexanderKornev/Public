00fe2c0b14bde0affe3c86103a87a2f841e57629
Alexander Kornev <headholder25@gmail.com>
2020-08-01 17:23:42 +0300

    Красивый вывод.

--- lesson_005/01_inhabitants.py
+++ lesson_005/01_inhabitants.py
@@ -7,6 +7,6 @@ from os.path import join
 import lesson_005.room_1 as r1
 import lesson_005.room_2 as r2
 comma = ', '
-print('В комнате room_1 живут:', s.join(r1.folks))
-print('В комнате room_2 живут:', s.join(r2.folks))
+print('В комнате room_1 живут:', comma.join(r1.folks))
+print('В комнате room_2 живут:', comma.join(r2.folks))
 
