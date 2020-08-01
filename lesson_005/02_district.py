e65270eb6ef62a6392d53a1809560b4e6743cc42
Alexander Kornev <headholder25@gmail.com>
2020-08-01 17:36:04 +0300

    Вывел на консоль через запятую всех жильцов района.

--- lesson_005/02_district.py
+++ lesson_005/02_district.py
@@ -5,4 +5,23 @@
 # подсказка: для вывода элементов списка через запятую можно использовать функцию строки .join()
 # https://docs.python.org/3/library/stdtypes.html#str.join
 
-# TODO здесь ваш код
+import lesson_005.district.central_street.house1.room1 as central_h1_r1
+import lesson_005.district.central_street.house1.room2 as central_h1_r2
+import lesson_005.district.central_street.house2.room1 as central_h2_r1
+import lesson_005.district.central_street.house2.room2 as central_h2_r2
+
+import lesson_005.district.soviet_street.house1.room1 as soviet_h1_r1
+import lesson_005.district.soviet_street.house1.room2 as soviet_h1_r2
+import lesson_005.district.soviet_street.house2.room1 as soviet_h2_r1
+import lesson_005.district.soviet_street.house2.room2 as soviet_h2_r2
+
+comma = ', '
+print('На районе живут:'
+      , comma.join(central_h1_r1.folks)
+      , comma.join(central_h1_r2.folks)
+      , comma.join(central_h2_r1.folks)
+      , comma.join(central_h2_r2.folks)
+      , comma.join(soviet_h1_r1.folks)
+      , comma.join(soviet_h1_r2.folks)
+      , comma.join(soviet_h2_r1.folks)
+      , comma.join(soviet_h2_r2.folks))
