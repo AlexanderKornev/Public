b3ec786dcc85defbb1e7845fb4baa6648ad92e5e
Alexander Kornev <headholder25@gmail.com>
2020-08-04 10:17:34 +0300

    Создал переменную с суммой всех списоков жильцов.

--- lesson_005/02_district.py
+++ lesson_005/02_district.py
@@ -17,17 +17,6 @@ import district.soviet_street.house1.room2 as soviet_h1_r2
 import district.soviet_street.house2.room1 as soviet_h2_r1
 import district.soviet_street.house2.room2 as soviet_h2_r2
 
-# TODO, предлагаю вместо переменной comma сразу использовать её значение.
-#  И в идеале, сократить количество переменных в print, возможно лучше создать заранее переменную.
-#  Сложить списки. Потом в print произвести join.
-comma = ', '
-print('На районе живут:'
-      , comma.join(central_h1_r1.folks)
-      , comma.join(central_h1_r2.folks)
-      , comma.join(central_h2_r1.folks)
-      , comma.join(central_h2_r2.folks)
-      , comma.join(soviet_h1_r1.folks)
-      , comma.join(soviet_h1_r2.folks)
-      , comma.join(soviet_h2_r1.folks)
-      , comma.join(soviet_h2_r2.folks)
-      )
+all_citizen = central_h1_r1.folks + central_h1_r2.folks + central_h2_r1.folks + central_h2_r2.folks + \
+              soviet_h1_r1.folks + soviet_h1_r2.folks + soviet_h2_r1.folks + soviet_h2_r2.folks
+print('На районе живут:', ', '.join(all_citizen))
