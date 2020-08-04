0bb75c26f4ac56b7bb4858818106e25746be8814
Alexander Kornev <headholder25@gmail.com>
2020-08-04 09:56:58 +0300

    Убрал переменную и сразу добаляю запятую.

--- lesson_005/01_inhabitants.py
+++ lesson_005/01_inhabitants.py
@@ -2,13 +2,11 @@
 
 # Вывести на консоль жителей комнат (модули room_1 и room_2)
 # Формат: В комнате room_1 живут: ...
-from os.path import join
 
 import lesson_005.room_1 as r1
 import lesson_005.room_2 as r2
-# TODO, предлагаю вместо переменной comma сразу использовать её значение. Количество кода не поменяется,
-#  но избавимся от лишней переменной.
-comma = ', '
-print('В комнате room_1 живут:', comma.join(r1.folks))
-print('В комнате room_2 живут:', comma.join(r2.folks))
+
+
+print('В комнате room_1 живут:', ', '.join(r1.folks))
+print('В комнате room_2 живут:', ', '.join(r2.folks))
 
