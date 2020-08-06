fe6ad82c028ff5976375a19b9b11b54c9bcdf0bc
Alexander Kornev <headholder25@gmail.com>
2020-08-06 16:25:16 +0300

    Спасибо! Разобрался, теперь все стало понятно) как обычно, все очевидно! Сделал статичную версию и версию с корявенькой анимацией. Снежинки нормально сыпятся) В принципе, для себя хотел разобраться со снегопадом, теперь все стало понятно.

--- lesson_005/epoch_making_canvas/rainbow.py
+++ lesson_005/epoch_making_canvas/rainbow.py
@@ -1,17 +1,29 @@
 # -*- coding: utf-8 -*-
 import simple_draw as sd
 sd.resolution = 1800, 800
-
+from random import randint
 def rainbow(point_x=500, end_point_x=2000):
     rainbow_colors = (sd.COLOR_RED, sd.COLOR_ORANGE, sd.COLOR_YELLOW, sd.COLOR_GREEN,
                       sd.COLOR_CYAN, sd.COLOR_BLUE, sd.COLOR_PURPLE)
+    ran = randint(0, 1)
+    if ran == 0:
+        for _ in rainbow_colors :
+            point = sd.get_point(point_x, 1500)
+            end_point = sd.get_point(end_point_x, 300)
+            sd.line(start_point=point, end_point=end_point, color=_, width=9)
+            sd.sleep(0.1)
+            point_x += 10
+            end_point_x += 10
+    else:
+        for _ in rainbow_colors :
+            point = sd.get_point(point_x, 1500)
+            end_point = sd.get_point(end_point_x, 300)
+            sd.line(start_point=point, end_point=end_point, color=sd.invert_color(_), width=9)
+            sd.sleep(0.1)
+            point_x += 10
+            end_point_x += 10
 
-    for _ in rainbow_colors :
-        point = sd.get_point(point_x, 1500)
-        end_point = sd.get_point(end_point_x, 300)
-        sd.line(start_point=point, end_point=end_point, color=_, width=9)
-        point_x += 10
-        end_point_x += 10
+
 
 
 
