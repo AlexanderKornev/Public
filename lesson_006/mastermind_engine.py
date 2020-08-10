2b7316b3e5c1a33e693adbc61f6c24895727e37f
Alexander Kornev <headholder25@gmail.com>
2020-08-10 20:40:56 +0300

    Упроситл преобразование строчки в список с литералами. Разбил длинную строчку в if, добавив elif.

--- lesson_006/mastermind_engine.py
+++ lesson_006/mastermind_engine.py
@@ -6,39 +6,18 @@ from termcolor import cprint, colored
 
 
 def check_user_input():
-    #  Александр, теперь, давайте немного поправим эту часть.
-    #  1. Возвращать будет или False или список user_number_list (Список с Элементами всегда True). Так будет правильнее.
-    #  2. Проверки осуществим с помощью одного условного оператора if/elif/else.
-    #  Будем проверять "Количество неповторяющихся элементов в списке", "является ли числом", "начинается ли с нуля"
-    #  И "количество символов".
-    #  3. Тут же реализуем цикл while. Если пользователь ввёл некорректное число, повторяем цикл и просим ввести
-    #  число заново. Если корректное - возвращаем число для выхода из цикла.
-    # сейчас реализовал в соответствии с третьим пунктом. Не совсем понимаю, как можно без цикла IF/ELSE в
-    # основном модуле реализовать просьбу снова ввести число от пользователя. Сейчас число от пользователя я запрашиваю
-    #   внутри функции check_user_input, при этом выполняя условие третей тудушки "Если пользователь ввёл некорректное " \
-    # "число, повторяем цикл и просим ввести число заново." Но это расходится с принципом единственной ответственности.
-    #   Может я не так понял этот пункт? Как мне попросить ввести число пользователя из функции, если запрос на введение
-    # числа должен быть сделан из 01_mastermind?
-
-    user_number_list = []
     while True:
         user_input = input(colored('Введите число : ', color='yellow'))
-
-        # TODO, этот цикл можно упростить. Сложный получился.
-        #  попробуйте print(list('1234')) или так print(list(map(int, '1234')))
-        for value in range(len(user_input)):
-            user_number_list += [int(str(user_input)[value])]
-
-        # TODO, вот эту чать, лучше разбить на несколько.
-        #  Выходить из условного оператора if/else лучше с помощью return user_number_list. Чуть меньше будет кода.
-        if len(user_input) != 4 or user_number_list[0] == 0 or len(user_number_list) != len(set(user_number_list)):
-            cprint('Вы ввели неверное число! Число должно состоять из четырех неповторяющихся цифр и'
+        user_number_list = list(map(int, user_input))
+        if len(user_number_list) != 4:
+            cprint('Вы ввели неверное число! Число должно состоять из четырех цифр.', color='red')
+            continue
+        elif user_number_list[0] == 0 or len(user_number_list) != len(user_number_list):
+            cprint('Вы ввели неверное число! Число должно состоять из неповторяющихся цифр и'
                    ' не должно начинаться с нуля.', color='red')
-            user_number_list = []
             continue
         else:
-            break
-    return user_number_list
+            return user_number_list
 
 
 number_list = []
@@ -65,9 +44,4 @@ def check_the_number(user_number):
             animals['bulls'] += 1
         elif value in user_number:
             animals['cows'] += 1
-    # for i in range(4):
-    #     if number_list[i] == user_number[i]:
-    #         animals['bulls'] += 1
-    #     elif number_list[i] in user_number:
-    #         animals['cows'] += 1
     return animals
