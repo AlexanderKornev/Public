0d7bcc400c289e4c9e19b31c79dd6f3c99cb1722
Alexander Kornev <headholder25@gmail.com>
2020-08-13 21:39:42 +0300

    Пока без пятой стихии.

--- lesson_007/02_alchemy.py
+++ lesson_007/02_alchemy.py
@@ -13,13 +13,108 @@
 
 # Сложение элементов реализовывать через __add__
 # Если результат не определен - то возвращать None
-# Вывод элемента на консоль реализовывать через __str__
+# Вывод элемента на консоль реализовывать через __init__
 #
 # Примеры преобразований:
 #   print(Water(), '+', Air(), '=', Water() + Air())
 #   print(Fire(), '+', Air(), '=', Fire() + Air())
 
-# TODO здесь ваш код
+
+
+
+class Water():
+    def __str__(self):
+        return 'Вода'
+
+    def __add__(self, other):
+        if isinstance(other, Wind):
+            return Storm()
+        if isinstance(other, Fire):
+            return Steam()
+        if isinstance(other, Ground):
+            return Mud()
+
+class Wind():
+    def __str__(self):
+        return 'Воздух'
+
+    def __add__(self, other):
+        if isinstance(other, Fire):
+            return Lightning()
+        if isinstance(other, Ground):
+            return Dust()
+
+class Fire():
+    def __str__(self):
+        return 'Огонь'
+    def __add__(self, other):
+        if isinstance(other, Ground):
+            return Lava()
+
+class Ground():
+    def __str__(self):
+        return 'Земля'
+
+class Storm():
+    def __str__(self):
+        return 'ШТОООООРРРМ'
+
+class Steam():
+    def __str__(self):
+        return 'Gabe Newell'
+
+class Mud():
+    def __str__(self):
+        return 'Грязь!!!'
+
+class Lightning():
+    def __str__(self):
+        return 'Молния!!!'
+
+class Dust():
+    def __str__(self):
+        return 'пыль...'
+
+class Lava():
+    def __str__(self):
+        return 'ЛАВА!!!!'
+    
+
+#   Вода + Воздух = Шторм
+#   Вода + Огонь = Пар
+#   Вода + Земля = Грязь
+#   Воздух + Огонь = Молния
+#   Воздух + Земля = Пыль
+#   Огонь + Земля = Лава
+print(Water(), '+', Wind(), '=', Water() + Wind())
+print(Water(), '+', Fire(), '=', Water() + Fire())
+print(Water(), '+', Ground(), '=', Water() + Ground())
+print(Wind(), '+', Fire(), '=', Wind() + Fire())
+print(Wind(), '+', Ground(), '=', Wind() + Ground())
+print(Fire(), '+', Ground(), '=', Fire() + Ground())
+
+
+
+class Could:
+    def __str__(self):
+        return 'ХОООЛОД'
+
+    def __add__(self, other):
+        if isinstance(other, Water):
+            return Ice()
+        if isinstance(other, Wind):
+            return
+        if isinstance(other, Fire):
+            return
+        if isinstance(other, Water):
+            return
+
+
+
+class Ice:
+    def __str__(self):
+        return 'Ice, ice, baby'
+
 
 # Усложненное задание (делать по желанию)
 # Добавить еще элемент в игру.
